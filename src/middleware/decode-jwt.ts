eeb9811f5b015784fd7edf5e9a5ce690251143cc
glennfaison <glennfaison@gmail.com>
2019-10-26 20:18:08 +0100

    Set more appropriate response status codes

--- src/middleware/decode-jwt.ts
+++ src/middleware/decode-jwt.ts
@@ -1,13 +1,13 @@
 import * as jsonWebToken from 'jsonwebtoken';
 import { config } from '../config';
-import {Response} from 'express';
+import { Response } from 'express';
 
 export function decodeJwt(req, res: Response, next) {
   try {
     let jwt = req.headers['authorization'];
     jwt = jwt!.split(' ')[1];
     req.me = jsonWebToken.verify(jwt, config.jwt.secret);
-  } catch (e) { }
+  } catch (e) { res.sendStatus(401); }
 
   next();
 };
